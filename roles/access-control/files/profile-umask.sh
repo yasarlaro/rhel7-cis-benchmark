cec8514e9be0a7178064ad15a3c0d0965f3ee188
Onur Yasarlar <yasarlar_onur@hotmail.com>
2019-09-13 19:13:48 +0400

    additional roles added

---
+++ roles/access-control/files/profile-umask.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/sh
+
+var_accounts_user_umask="027"
+
+grep -q umask /etc/profile && \
+  sed -i "s/umask.*/umask $var_accounts_user_umask/g" /etc/profile
+if ! [ $? -eq 0 ]; then
+    echo "umask $var_accounts_user_umask" >> /etc/profile
+fi
