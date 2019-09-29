fc447ca613e6e3b732ac0207fbd696cb73b434a3
Onur Yasarlar <yasarlar_onur@hotmail.com>
2019-09-29 20:23:54 +0400

    chapter work initial structure

--- roles/access-control/files/bash-umask.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/sh
-
-var_accounts_user_umask="027"
-
-grep -q umask /etc/bashrc && \
-  sed -i "s/umask.*/umask $var_accounts_user_umask/g" /etc/bashrc
-if ! [ $? -eq 0 ]; then
-    echo "umask $var_accounts_user_umask" >> /etc/bashrc
-fi
